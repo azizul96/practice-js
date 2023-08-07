0c424be15c83733ecb57b2250a368130d17d7662
Azizul <alifalamin42@gmail.com>
2023-08-07 19:33:34 +0600

    Problems added

--- problems.js
+++ problems.js
@@ -94,7 +94,7 @@ Input: -15
 Output: -22
 Input: 15
 Output: 30
-*/
+
 
 function isLGSeven(number){
     if(typeof number !== "number"){
@@ -113,7 +113,127 @@ function isLGSeven(number){
 }
 const inputNumber = isLGSeven(15)
 console.log(inputNumber);
-
+*/
 
 
 // Problem-5-------------------
+/*
+তোমাকে একটা function দেওয়া হবে called findingBadData(). এটা ইনপুট হিসেবে একটা array নিবে। array তে যেকোনো সংখ্যক কতগুলো number থাকবে। number গুলো negative number (less than zero) ও হতে পারে, আবার positive number (greater than or equal to zero) ও হতে পারে। কোনও number যদি negative হয় সেটাকে আমরা বলব “Bad Data”. কোনও number যদি positive হয় সেটাকে আমরা বলব “Good Data”. 
+এখন তোমার task: array তে কতগুলো Bad data আছে সেটা খুঁজে বের করতে হবে এবং সেই নাম্বার টা return করতে হবে।
+Sample Input & Output:-
+Input: [ 1,2,5 ]
+Output: 0
+Input: [ 2, -5, -7, -13 ]
+Output: 3
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Problem-6-------------------
+/*
+Problem 5: Convert your gems into diamond
+তোমাকে একটা function দেওয়া হবে called gemsToDiamond(). এটা ইনপুট হিসেবে তিনটা number ( তোমাদের ৩ বন্ধুর gems এর সংখ্যা ) নিবে। 
+১ম বন্ধুর প্রতি gems এর ক্ষমতা = 21
+২য় বন্ধুর প্রতি gems এর ক্ষমতা = 32
+৩য় বন্ধুর প্রতি gems এর ক্ষমতা = 43
+[ gems এর ক্ষমতা বলতে একটা gems কে কয়টা diamond এ convert করা যাবে সেটা বুঝানো হচ্ছে। ]
+এখন তোমার task: সব বন্ধুর gems মিলিয়ে total কতটি diamond পাবে সেটা বের করতে হবে। total diamond এর সংখ্যা যদি 1000 এর দিগুন এর উপর হয়ে যায় সেক্ষেত্রে total diamond থেকে 2000 বাদ দিয়ে যতগুলো diamond অবশিষ্ট থাকবে, ততগুলো তোমরা পাবে।
+Sample Input & Output:-
+Input: 1, 1, 1
+Output: 96
+Input: 20, 200, 50
+Output: 6970
+Input: 100, 5, 1
+Output: 303
+
+*/
+
+
+
+
+
+// Problem-7-------------------
+// function name printDetails(). you will be provided an object. you have to use the properties of the object and make a meaning full sentence. if there are any property missing you have to print there (nai);
+
+/*
+const obj = {
+    name: "mehedy",
+    age:26,
+    email:"abc@gmail.com"
+}
+Amar nam mehedy, amar boyos 26, amar email abc@gmail.com, amar bou nai. 
+*/
+
+
+
+
+// Problem-8-------------------
+/*
+Problem 1:
+আপনার কাছে একটি জাভাস্ক্রিপ্ট ফাংশন রয়েছে যার নাম হলো isValidPhotoName(photoName, imageExtensions)। এই ফাংশনটি দুটি প্যারামিটার গ্রহণ করে: photoName, যা একটি ছবির নাম প্রদর্শন করে, এবং imageExtensions Array, যা .jpg, .png, ইত্যাদি ছবির এক্সটেনশন গুলি ধারণ করে।
+আপনার কাজ হলো isValidPhotoName ফাংশনটি সম্পূর্ণ করা যাতে এই ফাংশনের মাধ্যমে পরীক্ষা করা যায় আপনি দেওয়া প্যারামিটার photoName সঠিক ছবির নাম কিনা তা। এই ফাংশনের কাজ হলো true রিটার্ন করা যদি photoName একটি বৈধ ছবির নামের শেষে বিদ্যমান ছবির এক্সটেনশন গুলির মধ্যে যেকোনটি শেষ হয়; অন্যথায়, ফাংশনটি false রিটার্ন করবে।
+
+*/
+
+
+
+
+
+// Problem-9-------------------
+/* function name sumOfN(). you will be provided a number n. you have to calculate the total of all numbers between 1 to n . use function recursion
+*/
+
+
+
+
+
+
+
+// Problem-10-------------------
+/*
+you will be provided an array of numbers. you have to return array of prime numbers from that input array. 
+
+
+
+function findPrimeNumbers(arr){
+    if(!Array.isArray(arr)){
+        return "please provide me an array of numbers"
+    }else{
+        
+        // 0,1 can not be prime number
+        let primes = [];
+        for(let item of arr){
+            if(item > 1){ //4 
+                let isPrime = true;
+
+                for(let i = 2; i< item; i++){ 
+                    if(item % i === 0){
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if(isPrime === true){
+                    primes.push(item)
+                }
+            }
+
+        }
+        return primes
+    }
+
+}
+
+const numbers = [ 4,8,5,10,19]
+
+console.log(findPrimeNumbers(numbers))
+*/
